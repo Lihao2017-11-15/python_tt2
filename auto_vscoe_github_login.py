4403dec11b0605e486953efbd78a153371e5f5e8
lihao2017-11-15 <812256@qq.com>
2020-12-03 14:38:32 +0800

    自动输入时禁用鼠标键盘

--- auto_vscoe_github_login.py
+++ auto_vscoe_github_login.py
@@ -1,5 +1,4 @@
-# -*- coding: UTF-8 -*-
-from ctypes import *  # 获取屏幕上某个坐标的颜色
+from ctypes import * 
 from random import *
 from inc.FormAPI import *
 from inc.kmAPI import *
@@ -19,23 +18,28 @@ def auto_login(username, password):
             if form.getWinTitle() != win_title:
                 continue
             else:
-                change_keyboard(Lan.EN)  # 切换为英文输入法
-                auto.typewrite(username)
-                auto.press('tab')
-                auto.typewrite(password)
-                auto.press('enter')
-                change_keyboard(Lan.ZH)
+                try:
+                    windll.user32.BlockInput(1) # 禁用鼠标键盘
+                    change_keyboard(Lan.EN)  # 切换为英文输入法
+                    auto.typewrite(username)
+                    auto.press('tab')
+                    auto.typewrite(password)
+                    auto.press('enter')
+                    change_keyboard(Lan.ZH)
+                finally:
+                    windll.user32.BlockInput(0) # 启用鼠标键盘
 
 
 #参数类使用实例
 if __name__ == "__main__":
     # https://docs.python.org/zh-cn/dev/library/argparse.html
+    print("在管理员权限下运行才可以禁止键盘鼠标干扰")
     import argparse
     parser = argparse.ArgumentParser(description='VS插件GitHub自动登录')
     parser.add_argument('-u', '--username', default="812256@qq.com", help="帐号")
-    parser.add_argument('-p', '--password', default="", help='密码')
+    parser.add_argument('-p', '--password', default="", help='密码', required=True)
     args = parser.parse_args()
     print(args)
-    input('press any key to continue...')
+    print("程序运行中...")
 
     auto_login(args.username, args.password)
